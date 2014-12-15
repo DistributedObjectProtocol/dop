63d4ac58ecde29738b6242bea3db9378da261a2d
Josenzo <enzo@josema.es>
2014-12-15 22:38:46 +0100

    Making nodejs part

--- sangular/lib/sangular.js
+++ sangular/lib/sangular.js
@@ -1,58 +1,76 @@
 
 (function() {
 
-	var sockjs = require('sockjs');
 
-	var App = function (app_name, options) {
+	var sangular_obj = {};
+
+	sangular_obj.sockjs = require('sockjs');
+
+	sangular_obj.decode  = function( string ) {
+
+		/*return ( decoded = string.match(/([0-9]+)(\w.*)/i) ) ?
+			decoded.slice(1)
+		:
+			string.match(/([^:]+):(.+)/i).slice(1);*/
+
+		return string;
+
+	};
+
+	sangular_obj.createApp = function(app_name, options) {
+
+		return new app(app_name, options);
+
+	};
+
+
+	var app = function (app_name, options) {
 
 		if (typeof app_name !== 'string' || app_name === '')
-			app_name = 'sangular';
+			app_name = '/sangular';
 
-		var server = sockjs.createServer( options );
+
+		var server = sangular_obj.sockjs.createServer( options );
 			server.clients = [];
-		
-		var value,
-			get,
-			decoded;
 
 
-		decode = function( string ) {
-
-			return ( decoded = string.match(/([0-9]+)(\w.*)/i) ) ?
-				decoded.slice(1)
-			:
-				string.match(/([^:]+):(.+)/i).slice(1);
-
-		};
 
 		server.on('connection', function(client) {
 
 			client.on('data', function( string ) {
 
-				decoded = decode( string );
+				var decoded = sangular_obj.decode( string );
 
-
-				if ( !isNaN(decoded[0]) ) { //Its a get, then
-
-					get = eval('global.' + decoded[1]);
-
-					if ( typeof get === 'function' )
-
-						get = get();
+				server.emit( 'data', string, decoded, client );
 
 
 
+				// if ( !isNaN(decoded[0]) ) { //Its a get, then
 
-					console.log(decoded, get)
+				// 	get = eval('global.' + decoded[1]);
+
+				// 	if ( typeof get === 'function' )
+
+				// 		get = get();
+
+
+
+
+				// 	console.log(decoded, get)
 					 
-				}
+				// }
 
-				//server.emit('data', client, decoded, string);
+				//
 
 				//console.log(message)
 				//(g|s):([^:]+):(.+)
 				//client.write(message);
 
+			});
+
+
+			client.on('close', function( string ) {
+				server.emit('close', client );
 			});
 
 		});
@@ -65,23 +83,18 @@
 
 				options = {};
 
-			options.prefix = '/' + app_name;
+			options.prefix = app_name;
 
 			server.installHandlers(httpserver, options);
 
-		}
+		};
 
 		return server;
 	};
 
 
 
-	module.exports.app = function(app_name, options) {
-
-		return new App(app_name, options);
-
-	};
-
+	module.exports = sangular_obj;
 
 
 })();
