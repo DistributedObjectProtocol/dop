7fa03f6ab9827721876c2e5ecd3bf4af14589b06
Josenzo <enzo@josema.es>
2016-08-06 19:35:40 +0800

    util.path and util.merge into one

--- src/util/path.js
+++ src/util/path.js
@@ -1,60 +1,68 @@
 
-// http://jsperf.com/stringify-path-vs-custom-path/2 - http://jsperf.com/stringify-path-vs-custom-path/3
-dop.util.path = function(obj, callback) {
+dop.util.path = function ( source, callback, destiny ) {
 
-    dop.util.pathRecursive.call({circular:[]}, obj, callback, []);
+    var hasCallback = typeof callback == 'function';
+     // UNCOMMENT THIS FOR STOP & REASIGN FEATURE
+    // if (hasCallback)
+        // config = {
+            // stop:function(){config.stoped=true},
+            // return:function(value){config.returned=value}
+        // };
+    // UNCOMMENT THIS FOR STOP & REASIGN FEATURE 
 
+    // dop.util.pathRecursive.call(config, [], source, destiny, callback, [], hasCallback, (destiny && typeof destiny=='object')); // UNCOMMENT THIS FOR STOP & REASIGN FEATURE
+    dop.util.pathRecursive(
+        [], 
+        source, 
+        destiny, 
+        callback, 
+        [], 
+        hasCallback,
+        (destiny && typeof destiny=='object')
+    );
+    return destiny;
 };
-dop.util.pathRecursive = function(obj, callback, path ) {
 
-    for (var key in obj) {
+dop.util.pathRecursive = function pathRecursive( circular, source, destiny, callback, path, hasCallback, hasDestiny ) {
 
-        //if ( obj.hasOwnProperty(key) ) {
-        
+    var key, value, value2, isArray;
+
+    for (key in source) {
+
+        if ( this.stoped === true ) return;
+
+        value = source[key];
+
+        if (hasCallback) {
             path.push( key );
+            callback(path.slice(0), source, destiny, this );
+            // if ( this.stoped === true ) return; // UNCOMMENT THIS FOR STOP & REASIGN FEATURE
+        }
 
-            if ( this.stop == false )
-                return;
+        // Objects or arrays
+        if ( value && value !== source && (value.constructor === Object || (isArray=Array.isArray(value))) && circular.indexOf(value)==-1 ) {
 
-            this.stop = callback(path, obj[key], key, obj);
+            circular.push(value);
 
-            // Avoiding circular loops
-            if ( obj[key] && typeof obj[key] == "object" && obj[key] !== obj && this.circular.indexOf(obj[key])==-1 ) {
-
-                this.circular.push(obj[key]);
-
-                dop.util.pathRecursive.call(this, obj[key], callback, path );
-
+            if ( hasDestiny ) {
+                value2 = ( !destiny.hasOwnProperty(key) ) ?
+                    (destiny[key] = (isArray) ? [] : {})
+                :
+                    destiny[key];
             }
 
+            pathRecursive(circular, value, value2, callback, path, hasCallback, hasDestiny );
+
+        }
+        else if ( hasDestiny && value !== undefined ) {
+            // destiny[key] = (hasCallback && this.hasOwnProperty('returned')) ? this.returned : value; // UNCOMMENT THIS FOR STOP & REASIGN FEATURE
+            // delete this.returned; // UNCOMMENT THIS FOR STOP & REASIGN FEATURE
+            destiny[key] = value; // COMMENT THIS FOR STOP & REASIGN FEATURE
+        }
+
+
+        if (hasCallback)
             path.pop();
-
-        //}
-
     }
 
 };
-
-
-
-// // http://jsperf.com/stringify-clousured-or-not - 
-// dop.stringify_path = function(obj, callback) {
-
-//     var path = [];
-
-//     return JSON.stringify(obj, function(k,v){
-
-//         if (v !== obj) {
-
-//             while ( path.length>0 && dop.util.get(obj, path) !== this )
-//                 path.pop();
-
-//             path.push(k);
-
-//         }
-
-//         return (typeof callback == 'function') ? callback.call(this, k, v, path) : v;
-
-//     });
-
-// };
