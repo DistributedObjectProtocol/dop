564031deb6e4c49030106d0996d84c029f7aeb28
Josenzo <enzo@josema.es>
2016-11-22 15:17:32 +0800

    Implemented nodejs.connect

---
+++ src/env/nodejs/connect.js
@@ -0,0 +1,13 @@
+
+dop.connect = function(options) {
+
+    var args = Array.prototype.slice.call(arguments, 0);
+
+    if (dop.util.typeof(args[0]) != 'object')
+        options = args[0] = {};
+
+    if (typeof options.transport != 'function')
+        options.transport = require('dop-transports').connect.websocket;
+
+    return dop.core.connector(args);
+};
