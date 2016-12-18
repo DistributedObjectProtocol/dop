6ed5e89d401fc1c05d7f399d770cc8d6937bb8a3
Josenzo <enzo@josema.es>
2016-12-18 17:28:53 +0800

    Making multiencoder

--- src/core/protocol/encode.js
+++ src/core/protocol/encode.js
@@ -1,13 +1,14 @@
+dop={core:{}}
+var data = {
+    function: function(){},
+    NaN: NaN,
+    Infinity: -Infinity,
+    undefined: undefined,
 
-dop.core.encode = function(property, value) {
+};
+dop.core.encode1 = function(property, value) {
 
     var tof = typeof value;
-
-    if (tof == 'function')
-        return '~F';
-
-    if (tof == 'undefined') // http://stackoverflow.com/questions/17648150/how-does-json-parse-manage-undefined
-        return '~U';
 
     if (value === Infinity)
         return '~I';
@@ -21,6 +22,20 @@ dop.core.encode = function(property, value) {
     if (tof == 'object' && value instanceof RegExp)
         return '~R' + value.toString();
 
+    return value;
+
+};
+dop.core.encode2 = function(property, value) {
+
+    var tof = typeof value;
+    if (value === '~i')
+        return 'LOOOL';
+    if (tof == 'function')
+        return '~F';
+
+    if (tof == 'undefined') // http://stackoverflow.com/questions/17648150/how-does-json-parse-manage-undefined
+        return '~U';
+
     if (tof == 'string' && value[0] === '~') // https://jsperf.com/charat-vs-index/5
         return '~'+value;
 
@@ -28,6 +43,21 @@ dop.core.encode = function(property, value) {
 
 };
 
+
+
+encode = function(data, encoders) {
+    var length = encoders.length;
+    return JSON.stringify(data, function recursion(property, value, index) {
+        if (index === undefined)
+            index = 0;
+        if (index<length) {
+            value = encoders[index](property, value);
+            return (index<length-1) ? recursion(property, value, index+1) : value;
+        }
+    })
+}
+
+console.log(encode(data, [dop.core.encode1,dop.core.encode2]));
 
 
 // // Extending example
