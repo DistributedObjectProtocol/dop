01f9ee74c2dba55209e1c0567cf353ed8cc2ad54
Josenzo <enzo@josema.es>
2017-01-06 17:48:05 +0800

    Done basic implementation of patch

--- src/protocol/patch.js
+++ src/protocol/patch.js
@@ -16,23 +16,25 @@ dop.protocol.patch = function(node, object_id, patch) {
 };
 
 dop.protocol.onpatch = function(node, request_id, request) {
-    var object_id = request[1],
+    var object_id_owner = request[1],
+        object_id = node.owner[object_id_owner],
         version = request[2],
         patch = request[3],
         response = dop.core.createResponse(request_id),
         object_data = dop.data.object[object_id],
         object_node;
     
-    if (isObject(object_data) && isObject(object_data.node[node.token]) && object_data.node[node.token].owner===object_id) {
+    if (isObject(object_data) && isObject(object_data.node[node.token]) && object_data.node[node.token].owner===object_id_owner) {
         object_node = object_data.node[node.token];
+        // We apply patch
         if (object_node.owner_version+1 === version) {
-            // We apply patch
             object_node.owner_version += 1;
             collector = dop.collectFirst();
             dop.core.setActionFunction(object_data.object, patch);
             collector.emitAndDestroy();
             response.push(0);
         }
+        //
     }
     else
         response.push(dop.core.error.reject_remote.OBJECT_NOT_FOUND);
@@ -42,10 +44,13 @@ dop.protocol.onpatch = function(node, request_id, request) {
 
 dop.protocol._onpatch = function(node, request_id, request, response) {
     var rejection = response[0],
+        // object_id = request[2],
         promise = request.promise;
+
+
     if (rejection !== undefined) {
         if (rejection === 0) {
-            
+            // var object_node = dop.data.object[object_id].node[node.token];
             promise.resolve(response[1]);
         }
         else
