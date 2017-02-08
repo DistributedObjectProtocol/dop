2563be578253da98b717bafe6df452bfa38fb3a4
Josenzo <enzo@josema.es>
2017-02-08 12:17:31 +0800

    Added drag example

---
+++ examples/drag/static/index.js
@@ -0,0 +1,54 @@
+// dop
+var server = dop.connect()
+var circleServer = dop.register({});
+server.subscribe().into(circleServer)
+dop.observe(circleServer, function() {
+    // We shouldn't update position from others if we are alredy moving it on localy
+    if (!circle.mover) { 
+        circle.el.style.left = circleServer.x + 'px'
+        circle.el.style.top = circleServer.y + 'px'
+    }
+})
+// end dop
+
+
+
+var circle = makeDraggable('circle', function(x,y){
+    circle.el.style.left = x + 'px'
+    circle.el.style.top = y + 'px'
+    // sending to server
+    circleServer.move(x, y) 
+})
+var square = makeDraggable('square', function(x,y){
+    square.el.style.left = x + 'px'
+    square.el.style.top = y + 'px'
+})
+
+
+function makeDraggable(elementId, callback) {
+    var shape ={}
+    shape.el = document.getElementById(elementId)
+    shape.mover = false
+    shape.first = true
+    shape.el.onmousedown = function() {
+        shape.mover = true
+    }
+    shape.el.onmouseup = function() {
+        shape.mover = false
+        shape.first = true
+    }
+    shape.el.onmousemove = function(e) {
+        if (shape.mover) {
+            if (shape.first) {
+                shape.x = e.offsetX
+                shape.y = e.offsetY
+                shape.first = false
+            }
+            shape.posx = e.pageX - shape.x
+            shape.posy = e.pageY - shape.y
+            callback.call(this, shape.posx, shape.posy)
+        }
+    }
+
+    return shape
+}
