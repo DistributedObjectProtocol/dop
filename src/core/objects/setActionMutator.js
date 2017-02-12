538c94d300d0fea28047c393c4a2c4380dfe97fb
Josenzo <enzo@josema.es>
2017-02-12 12:49:18 +0800

    Fixing bug when setting action deeply if the type change from object to array

--- src/core/objects/setActionMutator.js
+++ src/core/objects/setActionMutator.js
@@ -1,97 +1,73 @@
 
-dop.core.setActionMutator = function(destiny, prop, value, typeofValue) {
+dop.core.setActionMutator = function(destiny, prop, value, typeofValue, path) {
 
-    // if (path.length > 1) {
+    var typeofDestinyParent = dop.util.typeof(destiny),
+        typeofDestiny = dop.util.typeof(destiny[prop]);
 
-        var typeofDestiny = dop.util.typeof(destiny[prop]);
+    // Array mutations
+    if (typeofValue=='object' && typeofDestiny=='array' && value.hasOwnProperty(dop.cons.DOP)) {
 
-        // Array mutations
-        if (typeofValue=='object' && typeofDestiny=='array' && value.hasOwnProperty(dop.cons.DOP)) {
+        var mutations = value[dop.cons.DOP],
+            mutation,
+            index=0,
+            total=mutations.length,
+            typeArrayMutation;
 
-            var mutations = value[dop.cons.DOP],
-                mutation,
-                index=0,
-                total=mutations.length,
-                typeArrayMutation;
+        // if (typeofDestiny!='array')
+        //     dop.set(destiny, prop, []);
 
-            // if (typeofDestiny!='array')
-            //     dop.set(destiny, prop, []);
-
-            for (;index<total; ++index) {
-                typeArrayMutation = mutations[index][0]; // 0=swaps 1=splices
-                mutation = mutations[index].slice(1);
-                // swap
-                if (typeArrayMutation===0) {
-                    // mutation = mutation.slice(0);
-                    // (mutation[0]<0) ? mutation[0] = mutation[0]*-1 : mutation[1] = mutation[1]*-1;
-                    dop.core.swap(destiny[prop], mutation);
-                }
-                // set
-                else {
-                    // We have to update the length of the array in case that is lower than before
-                    if (destiny[prop].length<mutation[0])
-                        dop.getObjectTarget(destiny[prop]).length = mutation[0];
-                    // set
-                    if (mutation.length===3 && mutation[1]===1) {
-                        (mutation[2] === undefined) ?
-                            dop.del(destiny[prop], mutation[0])
-                        :
-                            dop.set(destiny[prop], mutation[0], mutation[2]);
-                    }
-                    // splice
-                    else
-                        dop.core.splice(destiny[prop], mutation);
-                }
+        for (;index<total; ++index) {
+            typeArrayMutation = mutations[index][0]; // 0=swaps 1=splices
+            mutation = mutations[index].slice(1);
+            // swap
+            if (typeArrayMutation===0) {
+                // mutation = mutation.slice(0);
+                // (mutation[0]<0) ? mutation[0] = mutation[0]*-1 : mutation[1] = mutation[1]*-1;
+                dop.core.swap(destiny[prop], mutation);
             }
-
-            if (typeof value.length == 'number' && value.length>-1)
-                destiny[prop].length = value.length;
-
-
-            return true; // Skiping to dont go inside of {~dop:...}
+            // set
+            else {
+                // We have to update the length of the array in case that is lower than before
+                if (destiny[prop].length<mutation[0])
+                    dop.getObjectTarget(destiny[prop]).length = mutation[0];
+                // set
+                if (mutation.length===3 && mutation[1]===1) {
+                    (mutation[2] === undefined) ?
+                        dop.del(destiny[prop], mutation[0])
+                    :
+                        dop.set(destiny[prop], mutation[0], mutation[2]);
+                }
+                // splice
+                else
+                    dop.core.splice(destiny[prop], mutation);
+            }
         }
 
-        else {
-
-            // // Set object and skip path deep
-            // if (typeofValue=='object' && typeofDestiny=='array' && !destiny.hasOwnProperty(prop)) {
-            //     dop.set(destiny, prop, dop.util.merge({}, value));
-            //     return true; // Skiping to dont go inside
-            // }
-
-            // // Set array and skip path deep
-            // else if (typeofValue=='array' && typeofDestiny=='object' && !destiny.hasOwnProperty(prop)) {
-            //     dop.set(destiny, prop, dop.util.merge([], value));
-            //     return true; // Skiping to dont go inside
-            // }
+        if (typeof value.length == 'number' && value.length>-1)
+            destiny[prop].length = value.length;
 
 
-            // else if (typeofValue=='object' && typeofDestiny!=typeofValue)
-            //     dop.set(destiny, prop, {});
+        return true; // Skiping to dont go inside of {~dop:...}
+    }
 
-            // else if (typeofValue=='array' && typeofDestiny!=typeofValue)
-            //     dop.set(destiny, prop, []);
+    else if (path.length > 1) {
 
+        if (typeofValue=='object' && typeofDestiny!='object') //!destiny.hasOwnProperty(prop)
+            dop.set(destiny, prop, {});
 
-            if (typeofValue=='object' && typeofDestiny!='object')
-                dop.set(destiny, prop, {});
-
-            else if (typeofValue=='array' && typeofDestiny!='array')
-                dop.set(destiny, prop, []);
+        else if (typeofValue=='array' && typeofDestiny!='array')
+            dop.set(destiny, prop, []);
 
 
-            // Delete
-            else if (typeofValue=='undefined')
-                dop.del(destiny, prop);
+        // Delete
+        else if (typeofValue=='undefined')
+            dop.del(destiny, prop);
 
-            // Set value
-            else if (typeofValue!='object')
-                dop.set(destiny, prop, value);
+        // Set value
+        else if (typeofValue!='object')
+            dop.set(destiny, prop, value);
 
-
-
-        }
-    // }
+    }
 };
 // dop.core.setActionLoop = function() {
 //     if (prop === dop.cons.DOP)
