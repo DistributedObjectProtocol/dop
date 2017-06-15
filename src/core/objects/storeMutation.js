756cca2bfd96441d882d0a7185840a363228e2f5
Josenzo <enzo@josema.es>
2017-06-15 20:32:44 +0200

    We update computed values after each recalculation to observe new derivations

--- src/core/objects/storeMutation.js
+++ src/core/objects/storeMutation.js
@@ -3,25 +3,23 @@ dop.core.storeMutation = function(mutation) {
 
     var collectors = dop.data.collectors,
         index=0,
-        total=collectors.length;
+        total=collectors.length,
+        path_id;
 
     // Saving path_id
-    mutation.path_id = dop.core.getPathId(
-        (mutation.splice!==undefined || mutation.swaps!==undefined) ?
-            mutation.path
-        :
-            mutation.path.concat(mutation.prop)
-    );
+    path_id = mutation.path_id = dop.core.getPathId(mutation.path);
 
+    if (mutation.splice===undefined && mutation.swaps===undefined)
+        path_id += dop.core.pathSeparator(mutation.prop);
 
     // Running collectors
     for (;index<total; index++)
         if (collectors[index].add(mutation))
-            return dop.core.runDerivations(mutation.path_id);
+            return dop.core.runDerivations(path_id);
 
 
     var snapshot = new dop.core.snapshot([mutation]);
     snapshot.emit();
 
-    dop.core.runDerivations(mutation.path_id);
+    dop.core.runDerivations(path_id);
 };
