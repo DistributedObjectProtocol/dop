ccf3a7ce5135ec4332b6273e38850695f11afd05
Josema <enzo@josema.es>
2018-08-28 09:46:48 +0200

    Refactoring patch of Satyam Singh

--- src/core/protocol/unregisterNode.js
+++ src/core/protocol/unregisterNode.js
@@ -1,16 +1,23 @@
 dop.core.unregisterNode = function(node) {
     var object_id, object_owner_id, object_data
-    // Removing subscriber objects
+
     for (object_id in node.subscriber) {
         object_data = dop.data.object[object_id]
-        if (
-            object_data !== undefined &&
-            object_data.node[node.token] !== undefined
-        ) {
-            object_data.nodes_total -= 1
-            delete object_data.node[node.token]
+        if (object_data !== undefined) {
+            // Removing subscriber objects
+            if (object_data.node[node.token] !== undefined) {
+                object_data.nodes_total -= 1
+                delete object_data.node[node.token]
+            }
+
+            // Simply remove all associated objects for which this node has subscription
+            // only if object node_total ==0; Added by Satyam Singh.
+            if (object_data.nodes_total === 0) {
+                delete dop.data.object[object_id]
+            }
         }
     }
+
     // Removing owner objects
     for (object_owner_id in node.owner) {
         object_id = node.owner[object_owner_id]
@@ -23,19 +30,7 @@ dop.core.unregisterNode = function(node) {
             delete object_data.node[node.token]
         }
     }
-       
-    // Deleting object data if not more nodes are depending
-        /*if (object_data!==undefined && object_data.nodes_total === 0)
-            delete dop.data.object[object_id];*/
 
-    //Added by Satyam Singh. Simply remove all associated objects for which this node has subscription
-    //only if object node_total ==0;
-
-    for (object_id in node.subscriber) {
-        object_data = dop.data.object[object_id];
-        if (object_data !== undefined && object_data.nodes_total === 0)
-            delete dop.data.object[object_id];
-    }
-
+    // Deleting node
     delete dop.data.node[node.token]
 }
