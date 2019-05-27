287d1cba2d44a162ee6ad19c2e574c5a74787212
Josema <enzo@josema.es>
2019-05-27 17:23:16 +0200

    Better protocol/connect

--- test/protocol/.connect.js
+++ test/protocol/.connect.js
@@ -3,7 +3,7 @@ const transportName = process.argv[2] || 'local'
 const transportListen = require('dop-transports').listen[transportName]
 const transportConnect = require('dop-transports').connect[transportName]
 
-function connect(t, end = true, port2 = 56724) {
+function connect(t, end = true, port1 = 12345, port2 = 23456) {
     return new Promise(function(response, reject) {
         const dopServer = dop.create()
         const dopClient = dop.create()
@@ -11,16 +11,20 @@ function connect(t, end = true, port2 = 56724) {
         dopServer.env = 'SERVER'
         dopClient.env = 'CLIENT'
         dopSubClient.env = 'SUBCLIENT'
-        const transportServer = dopServer.listen({ transport: transportListen })
-        const transportClientListen = dopClient.listen({
+        const transportServer = dopServer.listen({
             transport: transportListen,
-            port: port2
+            port: port1
         })
         const transportClient = dopClient.connect({
-            transport: transportConnect
+            transport: transportConnect,
+            url: 'ws://localhost:' + port1
         })
 
         transportClient.on('connect', function(nodeClient) {
+            const transportClientListen = dopClient.listen({
+                transport: transportListen,
+                port: port2
+            })
             const transportSubClient = dopSubClient.connect({
                 transport: transportConnect,
                 url: 'ws://localhost:' + port2
