114f98cd909e6b901dfd79e7487eace50f5c3899
Josema <enzo@josema.es>
2019-06-26 13:16:18 +0200

    Fixing issue with versining

--- src/env/browser/websocket.js
+++ src/env/browser/websocket.js
@@ -1,11 +1,9 @@
 ;(function(root) {
-    function websocket(dop, node, options) {
-        var url = 'ws://localhost:4444/' + dop.name,
-            oldSocket
-
-        if (typeof options.url == 'string')
+    function websocket(dop, options) {
+        var url = 'ws://localhost:4444/' + dop.name
+        if (typeof options.url == 'string') {
             url = options.url.replace('http', 'ws')
-        else if (
+        } else if (
             typeof window != 'undefined' &&
             typeof window.location != 'undefined' &&
             /http/.test(window.location.href)
@@ -22,104 +20,40 @@
                 dop.name
         }
 
-        // Variables
-        var api = options.transport.getApi(),
-            socket = new api(url),
-            tokenServer,
-            send_queue = [],
-            readyState
-
-        // Helpers
-        function send(message) {
-            socket.readyState === OPEN
-                ? socket.send(message)
-                : send_queue.push(message)
-        }
-        function sendQueue() {
-            if (socket.readyState === OPEN)
-                while (send_queue.length > 0) socket.send(send_queue.shift())
-        }
-
-        // Socket events
-        function onopen() {
-            // Reconnect
-            if (readyState === CONNECTING) socket.send(tokenServer)
-            // Connect
-            else {
-                socket.send('') // Empty means we want to get connected
-                readyState = OPEN
+        var transport = dop.createTransport()
+        var WebSocket = options.transport.getApi()
+        ;(function reconnect() {
+            var socket = new WebSocket(url)
+            function send(message) {
+                if (socket.readyState === 1) {
+                    socket.send(message)
+                    return true
+                }
+                return false
             }
-            dop.core.emitOpen(node, socket, options.transport)
-        }
-        function onmessage(message) {
-            // console.log( 'C<<: `'+message.data+'`' );
-            // Reconnecting
-            if (readyState === CONNECTING && message.data === tokenServer) {
-                readyState = CONNECT
-                dop.core.setSocketToNode(node, socket)
-                dop.core.emitReconnect(node, oldSocket)
-                sendQueue()
-            } else if (readyState !== CONNECT) {
-                tokenServer = message.data
-                readyState = CONNECT
-                dop.core.setSocketToNode(node, socket)
-                send(tokenServer)
-                sendQueue()
-                dop.core.emitConnect(node)
-            } else dop.core.emitMessage(node, message.data, message)
-        }
-        function onclose() {
-            readyState = CLOSE
-            dop.core.emitClose(node, socket)
-            dop.core.emitDisconnect(node)
-        }
-
-        // dop events
-        // function onconnect() {
-        //     if (readyState === CONNECTING) {
-        //         dop.core.emitDisconnect(node);
-        //         dop.core.setSocketToNode(node, socket);
-        //     }
-        //     readyState = CONNECT;
-        //     dop.core.emitConnect(node);
-        //     sendQueue();
-        // }
-        function ondisconnect() {
-            readyState = CLOSE
-            socket.close()
-        }
-
-        function reconnect() {
-            if (readyState === CLOSE) {
-                oldSocket = socket
-                socket = new api(url)
-                readyState = CONNECTING
-                addListeners(socket, onopen, onmessage, onclose)
-                removeListeners(oldSocket, onopen, onmessage, onclose)
+            function disconnect() {
+                socket.close()
             }
-        }
+            var node = transport.onCreate(socket, send, disconnect)
+            socket.addEventListener('open', function() {
+                transport.onConnect(node)
+            })
+            socket.addEventListener('message', function(message) {
+                transport.onMessage(node, message.data)
+            })
+            socket.addEventListener('close', function() {
+                transport.onDisconnect(node)
+            })
+            socket.addEventListener('error', function(error) {
+                transport.onError(node, error)
+            })
+        })()
 
-        // Setting up
-        dop.core.setSocketToNode(node, socket)
-        readyState = CLOSE
-        node.reconnect = reconnect
-        // node.on(dop.cons.CONNECT, onconnect);
-        node.on(dop.cons.SEND, send)
-        node.on(dop.cons.DISCONNECT, ondisconnect)
-        addListeners(socket, onopen, onmessage, onclose)
-
-        return socket
+        return transport
     }
 
-    function addListeners(socket, onopen, onmessage, onclose) {
-        socket.addEventListener('open', onopen)
-        socket.addEventListener('message', onmessage)
-        socket.addEventListener('close', onclose)
-    }
-    function removeListeners(socket, onopen, onmessage, onclose) {
-        socket.removeEventListener('open', onopen)
-        socket.removeEventListener('message', onmessage)
-        socket.removeEventListener('close', onclose)
+    websocket.getApi = function() {
+        return window.WebSocket
     }
 
     // UMD
@@ -131,9 +65,9 @@
             typeof factory == 'function' &&
             dop.create === factory
         ) // this is true if we are inside of dop.factory
-    )
+    ) {
         module.exports = websocket
-    else {
+    } else {
         websocket.getApi = function() {
             return window.WebSocket
         }
@@ -141,10 +75,4 @@
             ? (dop.transports.connect.websocket = websocket)
             : (root.dopTransportsConnectWebsocket = websocket)
     }
-
-    // Cons
-    var CLOSE = 0,
-        OPEN = 1,
-        CONNECTING = 2,
-        CONNECT = 3
 })(this)
