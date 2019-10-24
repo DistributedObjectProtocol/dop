2ac51b5cd80133fb540e545eaa6635f466a03674
Josema <enzo@josema.es>
2019-10-24 15:35:05 +0200

    forEachObject implemented

--- src/util/forEachObject.js
+++ src/util/forEachObject.js
@@ -1,30 +1,46 @@
 import { isObject } from './is'
 
+// https://jsperf.com/dop-foreachobject
 export default function forEachObject(origin, callback, destiny) {
-    const has_destiny = isObject(destiny)
     const circular = []
     const path = []
-    ;(function recursive(origin, destiny) {
-        for (const prop in origin) {
-            const value_origin = origin[prop]
+    const has_destiny = isObject(destiny)
+    forEachObjectLoop(origin, destiny, callback, circular, path, has_destiny)
+}
 
-            path.push(prop)
+function forEachObjectLoop(
+    origin,
+    destiny,
+    callback,
+    circular,
+    path,
+    has_destiny
+) {
+    for (const prop in origin) {
+        path.push(prop)
 
-            const skip = callback({ origin, destiny, prop, path })
+        const value_origin = origin[prop]
+        const skip = callback({ origin, destiny, prop, path })
 
-            if (
-                isObject(value_origin) &&
-                skip !== true &&
-                value_origin !== origin &&
-                // (!has_destiny ||
-                //     (has_destiny && destiny[prop] !== undefined)) &&
-                circular.indexOf(value_origin) == -1
-            ) {
-                circular.push(value_origin)
-                recursive(value_origin, has_destiny ? destiny[prop] : undefined)
-            }
-
-            path.pop()
+        if (
+            isObject(value_origin) &&
+            skip !== true &&
+            value_origin !== origin &&
+            // (!has_destiny ||
+            //     (has_destiny && destiny[prop] !== undefined)) &&
+            circular.indexOf(value_origin) == -1
+        ) {
+            circular.push(value_origin)
+            forEachObjectLoop(
+                value_origin,
+                has_destiny ? destiny[prop] : undefined,
+                callback,
+                circular,
+                path,
+                has_destiny
+            )
         }
-    })(origin, destiny)
+
+        path.pop()
+    }
 }
