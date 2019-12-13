fb45e0a0806f02ae1ccddc4e23accaf8b2385707
Josema <enzo@josema.es>
2019-12-13 18:39:03 +0100

    Cleaning rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,9 @@
 import pkg from './package.json'
-// import resolve from 'rollup-plugin-node-resolve'
-// import babel from 'rollup-plugin-babel'
-// import commonjs from 'rollup-plugin-commonjs'
 import minify from 'rollup-plugin-babel-minify'
 import buble from '@rollup/plugin-buble'
 
 export default [
-    // browser-friendly UMD build
-    {
-        input: 'src/index',
-        output: {
-            name: 'dop',
-            file: pkg.browser,
-            // compact: true,
-            format: 'umd'
-        },
-        plugins: [
-            // resolve(), // so Rollup can find `ms`
-            // babel({
-            //     exclude: 'node_modules/**',
-            //     plugins: ['transform-es2015-arrow-functions']
-            // }),
-            // commonjs(), // so Rollup can convert `ms` to an ES module
-            buble(),
-            minify({ comments: false })
-        ]
-    },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
+    // dop.js
     {
         input: 'src/index',
         external: ['ms'],
@@ -41,5 +12,16 @@ export default [
             // { file: pkg.module, format: 'es' }
         ],
         plugins: [buble()]
+    },
+
+    // dop.umd.js
+    {
+        input: 'src/index',
+        output: {
+            name: 'dop',
+            file: pkg.browser,
+            format: 'umd'
+        },
+        plugins: [buble(), minify({ comments: false })]
     }
 ]
